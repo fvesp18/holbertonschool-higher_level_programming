4fa6b8828bb5277c0b30484317591b5a762f45ea
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-10 18:11:41 +0000

    Prints random digit and tells user if its negative, positive, or zero

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Assigns random signed number to var number """
+
+
+import random
+number = random.randint(-10000, 10000)
+
+number_if_pos = "{} is positive"
+number_if_zero = "{} is zero"
+number_if_neg = "{} is negative"
+
+if number == 0:
+    print(number_if_zero.format(number))
+if number > 0:
+    print(number_if_pos.format(number))
+if number < 0:
+    print(number_if_neg.format(number))
