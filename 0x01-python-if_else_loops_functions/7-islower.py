7dc35dc4e5949f5751fddc1409e96c46bd6fc056
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-11 00:42:07 +0000

    intial

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" specifies if a char is uppercase or lowercase """
+
+
+def islower(c):
+    if (c >= 97 and c <= 122):
+        return(True)
+    else:
+        return(False)
