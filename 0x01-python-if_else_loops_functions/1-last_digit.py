20f5a5dc3948a0d1bef2a57e76c40457e20bfcf2
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-11 03:24:57 +0000

    add more if statments and print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,18 @@ import random
 number = random.randint(-10000, 10000)
 
 sentence = "Last digit of {0} is {1} {2}"
-if number < 0:
-        ld = number * -1
 ld = number % 10
 five = "and is greater than 5"
 lessthan = "and is less than 6 and not 0"
 zero = "and is 0"
-print(sentence.format(number, ld, zero if ld == 0 else lessthan))
+if ld > 5:
+    print(sentence.format(number, ld, five))
+elif ld == 0:
+    print(sentence.format(number, ld, zero))
+else:
+    if number < 0:
+        new = (number * -1) % 10
+        print(sentence.format(number, new, lessthan * -1))
+    else:
+        print(sentence.format(number, new, lessthan))
+
