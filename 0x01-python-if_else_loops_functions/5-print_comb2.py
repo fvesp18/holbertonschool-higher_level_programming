7f9c884bdaa01e4d2cda0d7e81548401cebacc22
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-11 00:36:42 +0000

    separate each number by a , and a space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(00, 100):
     if i <= 98:
-        print("{:02d}".format(i), end=", ")
+        print("{:02d}, ".format(i), end="")
 
 if i == 99:
     print("{:d}".format(i))
