4b409f2528f19fc74caaddcacb5d42e694ff66cb
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-11 00:55:06 +0000

    converts and prints str to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" prints chars to uppercase """
+
+
+def uppercase(str):
+    for ch in str:
+        if (ord(ch) >= 97 and ord(ch) <= 122):
+            ch = ord(ch) - 32
+            print("{}".format(chr(ch)), end="")
+        else:
+            print("{}".format(ch), end="")
+    print("")
