f15b9d081e0ea0e51397b0606bb218bc7895f2c2
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-12 21:21:59 +0000

    Add if statement, fix formatting

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-i = 0
-length = len(argv)
-if length == 1:
-    length = length - 1
-    statement = "{} arguments."
-    print(statement.format(length))
-elif length > 1:
-    length = length - 1
-    statement = "{} arguments:"
-    print(statement.format(length))
-    while i < length:
-        i += 1
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    i = 0
+    length = len(argv)
+    if length == 1:
+        length = length - 1
+        statement = "{} arguments."
+        print(statement.format(length))
+    elif length > 1:
+        length = length - 1
+        statement = "{} arguments:"
+        print(statement.format(length))
+        while i < length:
+            i += 1
+            print("{}: {}".format(i, argv[i]))
