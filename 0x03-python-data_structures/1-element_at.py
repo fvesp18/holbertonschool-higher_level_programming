e6e73ef3bc63e85a41c0e648047c161e1178afd4
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:02:34 +0000

    move if statement into while loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,10 +7,10 @@ def element_at(my_list, idx):
     i = 0
     if idx < 0:
         return None
-    if idx > len(my_list):
-        return None
     else:
         while i < idx:
             i = i + 1
-            if i == idx:
+            if idx > len(my_list):
+                return None
+            elif i == idx:
                 return my_list[i]
