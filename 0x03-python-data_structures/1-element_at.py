09951581bad3b3169b3d73020dc272c7edc746c1
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:15:06 +0000

    prints element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,4 @@ def element_at(my_list, idx):
     if idx > len(my_list):
         return None
     else:
-        while i < idx:
-            i = i + 1
-            if i == idx:
-                return my_list[idx]
+        return my_list[idx]
