79fffc66142fc8f1ab1584513c1c3deae8a39f22
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:28:02 +0000

    return i

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,5 +6,5 @@ def element_at(my_list, idx):
         elif idx > len(my_list):
             return None
         else:
-            if i == idx:
-                return my_list[i]
+            if i == idx + 1:
+                return i
