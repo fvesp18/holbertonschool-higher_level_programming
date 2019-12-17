cf6e668d41d54fec8d5e53e5dd3415f6214fd2a6
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:25:15 +0000

    bring while loop out of else

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx > len(my_list):
-        return None
-    else:
-        while i in my_list:
+    for i in my_list:
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
             if i == idx:
                 return my_list[i]
