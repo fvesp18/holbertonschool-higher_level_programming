4df0f800f48e8e5627b12fa2408da1de6dfed9b6
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:03:55 +0000

    remove comment

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-""" program prints element from list at idx """
-
-
 def element_at(my_list, idx):
-    """ compares i to len(my_list) """
     i = 0
     if idx < 0:
         return None
