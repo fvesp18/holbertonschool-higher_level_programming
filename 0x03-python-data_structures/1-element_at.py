e16e280a6575019530995e5132fc70e67230e729
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:12:13 +0000

    change return from my_list[i] to my_list[idx]

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,10 +3,10 @@ def element_at(my_list, idx):
     i = 0
     if idx < 0:
         return None
+    if idx > len(my_list):
+        return None
     else:
         while i < idx:
             i = i + 1
-            if idx > len(my_list):
-                return None
-            elif i == idx:
-                return my_list[i]
+            if i == idx:
+                return my_list[idx]
