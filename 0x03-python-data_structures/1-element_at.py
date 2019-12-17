f5234ea50f591b1b3625e7fc4c150a555ebd615a
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-17 23:00:15 +0000

    change return statements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,11 +6,11 @@ def element_at(my_list, idx):
     """ compares i to len(my_list) """
     i = 0
     if idx < 0:
-        return
+        return None
     if idx > len(my_list):
-        return
+        return None
     else:
         while i < idx:
             i = i + 1
             if i == idx:
-                return my_list
+                return my_list[i]
