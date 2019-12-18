75f1eb862c42fee80a74510918603e1853ef1b28
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-18 02:17:00 +0000

    replaces element at idx with new element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,12 +2,14 @@
 
 
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    my_list.copy()
     if my_list:
         if idx < 0:
             return my_list
-        if idx > len(my_list):
+        if idx >= len(my_list) - 1:
             return my_list
-        if my_list[idx]:
-            my_list.insert(idx, element)
-            my_list.remove(idx + 1)
-            return my_list
+        if new_list[idx]:
+            new_list.remove(idx)
+            new_list.insert(idx, element)
+            return new_list
