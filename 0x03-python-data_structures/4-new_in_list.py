ce9c813032edc2e37f5f61ade6991dd18b873f56
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-18 02:20:34 +0000

    add one to idx in remove statement

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -10,6 +10,6 @@ def new_in_list(my_list, idx, element):
         if idx >= len(my_list) - 1:
             return my_list
         if new_list[idx]:
-            new_list.remove(idx)
+            new_list.remove(idx + 1)
             new_list.insert(idx, element)
             return new_list
