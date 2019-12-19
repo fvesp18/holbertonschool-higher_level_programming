5aaa7ef7b8bbc007495a45447a6d5c3a768401e7
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-19 04:37:11 +0000

    change range

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for i in range(len(matrix[row])):
-            if i != row + 3:
+            if i != row:
                 print("{:d} ".format(matrix[row][i]), end="")
 
         print("")
