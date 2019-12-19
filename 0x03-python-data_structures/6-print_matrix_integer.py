9af89fc74c0de38006ea8d87432ff968c23c891e
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-19 04:30:29 +0000

    no

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join(map(str, row)))
+    for row in range(len(matrix)):
+        for i in range(len(matrix[row])):
+            if i != row + 3:
+                print("{:d} ".format(matrix[row][i]), end="")
+
+        print("")
