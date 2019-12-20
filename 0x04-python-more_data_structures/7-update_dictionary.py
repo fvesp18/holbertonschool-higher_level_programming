31b2e69decbf72dcde66e945a964d24ccda207f1
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-20 04:55:45 +0000

    intial commit

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary[key]:
+        a_dictionary.update(key = value)
+    else:
+        a_dictionaru[key] = value
