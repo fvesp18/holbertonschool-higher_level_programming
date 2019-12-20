2aa3c57b28f71d636728e88621e98b626931ab66
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-12-20 16:17:57 +0000

    updates dictionary at key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,5 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        a_dictionary[key] = a_dictionary.update(a_dictionary[value])
-    else:
-        a_dictionaru[key] = value
+    a_dictionary[key] = value
+    return a_dictionary
