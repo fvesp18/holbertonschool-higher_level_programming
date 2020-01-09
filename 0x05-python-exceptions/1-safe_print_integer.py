a6f64a4e03abca318862694dfe32a1b258729ff8
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-09 01:40:34 +0000

    change condition in while loop

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    while value == isinstance(value, int):
+    while isinstance(value, int):
         try:
             print("{:d}".format(value))
             return True
