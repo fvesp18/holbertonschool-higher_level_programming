d33d6628eb1e2a01a78aea2e88efd10a9b6ddb10
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-09 02:46:30 +0000

    bring out return true

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        while isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
     except TypeError:
         return False
+    return True
