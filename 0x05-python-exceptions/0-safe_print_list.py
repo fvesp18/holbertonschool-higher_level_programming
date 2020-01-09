dea1f09081b2424c9d068715e189565c81b5dd0f
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-09 02:23:22 +0000

    bring loop inside try statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in my_list[:x]:
-        try:
-            print("{}".format(i), end="")
-        except IndexError:
-            return i
-    print("")
-    return x
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+        print("")
+        return x
+    except IndexError:
+        print("")
+        return i
+    print("\n")
