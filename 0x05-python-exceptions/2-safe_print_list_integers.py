c0e74bbe1246a75579afb9b4bdcbd9b5428312bf
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-09 03:34:34 +0000

    intial commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        my_list = [x for x in my_list if type(x) == int]
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+        print("")
+        return x
+    except TypeError:
+        return x
+    except IndexError:
+        print("")
+        return i
+    print("\n")
