2ee7128b4dbc82ee77bf667afd4a0415cab42068
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-09 01:36:59 +0000

    compare value to isinstance

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    while value in range(-1000, 1000):
+    while value == isinstance(value, int):
         try:
             print("{:d}".format(value))
             return True
