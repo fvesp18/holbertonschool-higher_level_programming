f8f988e097ed22708d4a8f00c731b2840b24867a
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-10 16:00:54 +0000

    fix indentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-	def __init__(self, size=0):
-		if size < 0:
-			raise ValueError("size must be >= 0")
-		elif type(size) != int:
-			raise TypeError("size must be an integer")
-		self.__size = size
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        self.__size = size
 
-	def area(self):
-		return self.__size*self.__size
+        def area(self):
+            return self.__size*self.__size
