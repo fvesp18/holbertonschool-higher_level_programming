a0def2ca4cabe17c58f9f0faa542634b25f31cd5
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-10 14:35:27 +0000

    change second except statement

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         return x
     except TypeError:
         return x
-    except IndexError:
+    except ValueError:
         print("")
         return i
     print("\n")
