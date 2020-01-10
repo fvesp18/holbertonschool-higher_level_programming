1fda8f8ef629312cf99027e77b1936c7c2a10a91
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-10 02:05:49 +0000

    add try: except:

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,5 +3,10 @@
 
 
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    try:
+        def __init__(self, size=0):
+            self.__size = size
+    except TypeError:
+        print("size must be an integer")
+    except ValueError:
+        print("size must be >= 0")
