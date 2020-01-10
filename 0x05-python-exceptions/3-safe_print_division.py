c90fb91740088ac16b520f82f48fb44be274e95f
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-10 14:42:23 +0000

    change value in .format

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: {}".format(a / b if b > 0 else None))
+        print("Inside result: {}".format(result))
     return result
