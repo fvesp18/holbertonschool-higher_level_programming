b03dd9c216eed29c8533d154fd64f3ce608fdedd
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-15 05:36:29 +0000

    fix for pep8

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -5,16 +5,17 @@
 class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
         self.number_of_instances += 1
         self.print_symbol
-        
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -22,6 +23,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         return self.__height
@@ -41,7 +43,7 @@ class Rectangle:
         if self.__width is 0 or self.__height is 0:
             return 0
         return 2 * (self.__width) + 2 * (self.__height)
-    
+
     def __str__(self):
         rec_rep = ''
         if self.__width is 0 or self.__height is 0:
@@ -50,12 +52,14 @@ class Rectangle:
             for i in range(self.__height):
                 rec_rep += (str(self.print_symbol)) * (self.__width) + ('\n')
             return rec_rep[:-1]
+
     def __repr__(self):
         return 'Rectangle(%d, %d)' % (self.__width, self.__height)
+
     def __del__(self):
         self.number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         area_1 = int(rect_1.area())
@@ -70,6 +74,7 @@ class Rectangle:
             return rect_1
         if area_2 > area_1:
             return rect_2
+
     @classmethod
     def square(cls, size=0):
         return Rectangle(size, size)
