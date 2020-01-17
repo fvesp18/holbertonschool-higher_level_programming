6e68a2c5354363797f4fe68fadffe7848f3a4d63
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-17 02:18:24 +0000

    fix pep8, spacing

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name """
+""" implement __slots__ for first name attribute """
 
 
 class LockedClass:
     __slots__ = "first_name"
+
     def __init__(self, first_name=None):
         self.first_name = first_name
