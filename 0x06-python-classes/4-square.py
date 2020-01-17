9553aed0a9613ae0e0e7e198565ab694ff706426
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-17 02:14:21 +0000

    check for push

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" writes class, defines square """
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" prints square """
+
+
+def my_print(size):
+    if size == 0:
+        print("")
+
+        for i in range(self.size):
+            print("#"*self.size)
