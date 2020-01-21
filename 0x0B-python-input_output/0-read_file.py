6a034c8355c89bdc2bfaf74ff11fdc885c3dfc86
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-21 23:25:13 +0000

    remove lineNum, format only aLine

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,6 @@
 
 
 def read_file(filename=""):
-    lineNum = 0
     with open('my_file_0.txt', encoding='UTF8') as aFile:
         for aLine in aFile:
-            lineNum += 1
-            print('{:}'.format(aLine.rstrip()))
+            print('{:}'.format(aLine), end='')
