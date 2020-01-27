fe13365a10d72be25dab62109e17a86e9196f932
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-27 20:46:56 +0000

    update update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,12 +17,32 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """ set size """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ size get """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.width = self.height = self.__size = value
+
+    def update(self, *args, **kwargs):
+        """ update """
+        if len(args) is not 0 and args is not None:
+            for position, arg in enumerate(args):
+                if position is 0:
+                    self.id = arg
+                if position is 1:
+                    self.__size = arg
+                if position is 2:
+                    self.x = arg
+                if position is 3:
+                    self.y = arg
+        else:
+            self.id = kwargs.get("id", self.id)
+            self.__size = kwargs.get("size", self.__size)
+            self.x = kwargs.get("x", self.x)
+            self.y = kwargs.get("y", self.y)
