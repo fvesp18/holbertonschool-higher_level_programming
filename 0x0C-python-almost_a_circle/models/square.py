99a02766fe5e14913483609b0277acb97f3c6005
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-27 20:24:45 +0000

    pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,10 +7,23 @@ class Square(Rectangle):
     """ initialize stuff """
     def __init__(self, size, x=0, y=0, id=None):
         """ load vars from Rect """
-        self.size = size
+        self.__size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ str decorator """
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.size))
+
+    @property
+    def size(self):
+        self.width = self.height = self.__size
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
