a8d92c1841b18502bbd2a24b32c173c0be877165
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-02 23:06:49 +0000

    [UPDATE] Prints description of first_table in hbtn_0c_0 db

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Creates a table called first_table in current db
+CREATE TABLE IF NOT EXISTS first_table (
+	id int NOT NULL,
+	name VARCHAR(256) );
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints full description of first_table in hbtn_0c_0
+USE hbtn_0c_0;
+SHOW CREATE TABLE first_table;
