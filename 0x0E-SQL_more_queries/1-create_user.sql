03aaaae108d123ebfb5d1a79d09f7c59716a2362
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-03 19:50:39 +0000

    [UPDATE] Grant privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Creates a user on a localhost called user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
