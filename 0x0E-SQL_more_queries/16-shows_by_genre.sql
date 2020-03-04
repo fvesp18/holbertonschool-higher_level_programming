12a0ecb05e8805c295badf85ff3dc67704a00ac5
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 19:20:28 +0000

    [UPDATE] Remove where statement

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,4 @@ LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name IS NULL
 ORDER BY tv_shows.title, tv_genres.name;
