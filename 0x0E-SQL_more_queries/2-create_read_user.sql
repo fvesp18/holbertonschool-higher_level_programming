79f6e9d67779efe7a58c575f250c7bf77d7e50ca
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 15:12:21 +0000

    [UPDATE] Add new line of comment

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- Creates a user and a database
+-- Grant select privileges to user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_d';
 GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
