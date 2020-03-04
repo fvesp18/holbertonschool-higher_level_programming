f5346fe2a42bea83af00af8706ff2c2cf20bace2
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 16:25:28 +0000

    [UPDATE] Add comment

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- Lists all shwos contained in htbn_0d_tvshows that have atleast one linked genre
+-- ANother line of comments
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows, hbtn_0d_tvshows.tv_show_genres
+FROM tv_shows, tv_show_genres
 WHERE tv_shows.id IS NOT NULL AND tv_shows.id = tv_show_genres.show_id
 GROUP BY tv_shows.title, tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
