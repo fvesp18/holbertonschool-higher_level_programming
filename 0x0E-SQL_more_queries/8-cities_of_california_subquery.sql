e50a76fad9880ddeb53f2c681b8f7665ae8568ec
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 15:55:57 +0000

    [UPDATE] Try references items by table

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- Creates database and table is same database
-USE hbtn_0d_usa;
-SELECT cities.id, cities.name FROM cities, states WHERE state.name = 'California' AND cities.id = ??? ORDER BY cities.id DESC;
+SELECT cities.id, cities.name FROM cities, states WHERE state.name = 'California' AND states.id = cities.state_id ORDER BY cities.id ASC;
