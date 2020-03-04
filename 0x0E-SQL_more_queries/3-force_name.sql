ce7c53c1dfb756249ab121d96572e6a22b4b3cda
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 15:17:02 +0000

    [UPDATE] Change to not null from is not null

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Creates a table called force_name on mysql server
 CREATE TABLE IF NOT EXISTS force_name (
-	id int IS NOT NULL,
-	name VARCHAR(256) IS NOT NULL);
+	id INT NOT NULL,
+	name VARCHAR(256) NOT NULL);
