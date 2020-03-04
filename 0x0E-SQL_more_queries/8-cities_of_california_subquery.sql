81ef9f7e36f6e2b8fe72d064c1164b941b995481
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 15:57:11 +0000

    [UPDATE] Add s to states in state.names

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Creates database and table is same database
-SELECT cities.id, cities.name FROM cities, states WHERE state.name = 'California' AND states.id = cities.state_id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name FROM cities, states WHERE states.name = 'California' AND states.id = cities.state_id ORDER BY cities.id ASC;
