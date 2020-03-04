0185a621aeb5d260edbe19421c4efd2a9be9cfe1
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 15:11:08 +0000

    [UPDATE] Create user, add if not exists

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- Creates a user and a database
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_d';
 GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
