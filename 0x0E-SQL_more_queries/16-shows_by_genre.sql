7e7e3d908b8954382688eecab9809243466383ed
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 19:07:37 +0000

    [UPDATE] Last two tasks

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Uses to htbn_0d_tvshows database to lists all genres of the show DEXTER
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name IS NULL
+ORDER BY tv_shows.title, tv_genres.name;
