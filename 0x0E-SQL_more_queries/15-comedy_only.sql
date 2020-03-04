2659d79f5bbe0ddb4e901297dfb480535d51b474
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-04 19:08:52 +0000

    [UPDATE] Edit where you SELECT from

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Uses to htbn_0d_tvshows database to lists all genres of the show DEXTER
-SELECT tv_genres.title
+SELECT tv_shows.title
 FROM tv_shows JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
