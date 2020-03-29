23114a43521fc385d9b612099e2cedbd6c489a49
fvesp18 <1081@holbertonschool.com>
2020-03-29 22:38:40 +0000

    [UPDATE] New

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Prints all states from a db by ascending id value """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            "localhost",
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3],
+            3306
+            )
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id;")
+    table = cur.fetchall()
+    for rows in table:
+        first_letter = rows[1]
+        if first_letter == sys.argv[4]:
+            print("{}".format(rows))
+    db.close()
