b5bfac5a16a31c7a834ab65b2d6cfb24a8a58827
fvesp18 <1081@holbertonschool.com>
2020-03-29 22:17:38 +0000

    [UPDATE] Add docstring

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,14 +5,14 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-            "127.0.0.1",
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3],
+            host="127.0.0.1",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
             port=3306)
     cur = db.cursor()
 
-    size = cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
     table = cur.fetchall()
     for rows in table:
         print("{}".format(rows))
