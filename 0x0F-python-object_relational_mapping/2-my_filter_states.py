a8d02d884b69a50df3942eee934c0dffb89322fd
fvesp18 <1081@holbertonschool.com>
2020-03-29 22:31:43 +0000

    [UPDATE] Do the thing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" Filter by user input """
+""" Prints all states from a db by ascending id value """
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-            "127.0.0.1",
+            "localhost",
             sys.argv[1],
             sys.argv[2],
             sys.argv[3],
-            port=3306)
+            3306
+            )
     cur = db.cursor()
 
-    size = cur.execute("SELECT * FROM states;")
-    for rows in range(size):
-        result = cur.fetchone()
-        string = result[1]
-        if string == sys.argv[4]:
-            print("{}".format(result))
+    cur.execute("SELECT * FROM states ORDER BY states.id;")
+    table = cur.fetchall()
+    for rows in table:
+        first_letter = rows[1]
+        if first_letter == sys.argv[4]:
+            print("{}".format(rows))
     db.close()
