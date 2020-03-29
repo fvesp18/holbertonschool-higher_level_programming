6d84ec9d01c908ae2c289a1727f32a6ddf4e9afc
fvesp18 <1081@holbertonschool.com>
2020-03-29 19:12:51 +0000

    [UPDATE] Connects to database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     table = cur.fetchall()
     for row in table:
-        for col in row:
-            print("({}, '{}')".format(state.id, state.name))
+        print("{}".format(row))
+    db.close
