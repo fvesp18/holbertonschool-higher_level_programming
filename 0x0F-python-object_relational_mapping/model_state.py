67ca1266264cb15a7c4df132b09b259696fba0d5
fvesp18 <1081@holbertonschool.com>
2020-03-30 18:14:25 +0000

    [UPDATE] Pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,11 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """ An honest comment in string literally """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 class State(Base):
+    """ Are you happy now, checker? """
     __tablename__ = 'states'
     id = Column(Integer(), nullable=False, primary_key=True, unique=True)
     name = Column(String(128), nullable=False)
