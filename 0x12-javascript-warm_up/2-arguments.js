2dcefed36e63a9f96d4a2e8ece8d10756549b7c1
fvesp18 <1081@holbertonschool.com>
2020-05-08 02:16:36 +0000

    [UPDATE] Checks length of CL args and returns statement

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const length = process.argv.slice(2).length
+
+if (length > 1) {
+	const str1 = 'Arguments found';
+	console.log(str1);
+} else if (length === 1) {
+	const str2 = 'Argument found';
+	console.log(str2);
+} else if (length === 0) {
+	const str3 = 'No argument';
+	console.log(str3);
+}
