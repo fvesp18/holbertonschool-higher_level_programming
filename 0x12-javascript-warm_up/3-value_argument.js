5c97773a6e04cb51690796385475980a74f65480
fvesp18 <1081@holbertonschool.com>
2020-05-08 02:40:03 +0000

    [UPDATE] Change == to ===

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const allArgs = process.argv.slice(2);
-if (allArgs[0] == undefined) {
+if (allArgs[0] === undefined) {
   console.log('No argument');
 } else {
   console.log(allArgs[0]);
