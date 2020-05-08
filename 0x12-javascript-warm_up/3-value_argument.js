d9b1267a7697a4b22751049e2fc6d151c1feb77e
fvesp18 <1081@holbertonschool.com>
2020-05-08 02:32:25 +0000

    [UPDATE] Make semistandard compliant

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,6 +3,6 @@ const allArgs = process.argv.slice(2);
 const length = allArgs.length;
 if (length === 0) {
   console.log('No Argument');
-} else if (allArgs)  {
+} else if (allArgs) {
   console.log(allArgs[0]);
 }
