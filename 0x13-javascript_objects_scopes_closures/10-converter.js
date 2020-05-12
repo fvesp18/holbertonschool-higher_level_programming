ee988af5c50a349a484fbbf3f38f0e86ebf9de40
fvesp18 <1081@holbertonschool.com>
2020-05-12 05:03:02 +0000

    [UPDATE] Task 10, add comment

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
+// Add a comment lmfao
 exports.converter = function (base) {
-	if (base === 10) {
-		return function (base) {
-			return base.toString(10);
-		};
-	} else {
-		return function (base) {
-			return base.toString(16);
-		};
-	}
+  if (base === 10) {
+    return function (base) {
+      return base.toString(10);
+    };
+  } else {
+    return function (base) {
+      return base.toString(16);
+    };
+  }
 };
