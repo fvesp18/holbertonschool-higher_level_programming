1f53b754328efc01592baf432dff760bff60d09f
fvesp18 <1081@holbertonschool.com>
2020-05-12 04:22:49 +0000

    [UPDATE] Task 6, print square using c ic undefined print X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+	c = "X";
+	let row = 0;
+	let col = 0;
+	let str = '';
+	for (; row < this.width; row++) {
+		for (; col < this.height; col++) {
+			str += c;
+		}
+		console.log(str);
+	}
+    } else {
+	let row = 0;
+	let col = 0;
+	let str = '';
+	for (; row <= this.width; row++) {
+		for (; col <= this.height; col++) {
+			str += c;
+		}
+		console.log(str);
+	}
+    }
+  }
+};
