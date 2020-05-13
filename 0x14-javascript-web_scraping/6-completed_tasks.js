e5e763069a6c02b238490fb3e26ad8d6411b2d0c
fvesp18 <1081@holbertonschool.com>
2020-05-13 02:18:20 +0000

    [UPDATE] Task 6, requests todos list from url, creates count of tasks completed by using loop and if conditions

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const req = require('request');
+const allArgs = process.argv.slice(2);
+const url = allArgs[0];
+
+req.get(url, function (err, r, cont) {
+  if (err) {
+    console.log(err);
+  } else {
+    let op = {};
+    let init = 0;
+    const tmp = JSON.parse(cont);
+    const len = tmp.length;
+    while (init < len) {
+      if (tmp[init].completed) {
+        if (!(tmp[init].userId in op)) {
+          op[tmp[init].userId] = 0;
+        }
+        op[tmp[init].userId] += 1;
+      }
+      init += 1;
+    }
+    console.log(op);
+  }
+});
