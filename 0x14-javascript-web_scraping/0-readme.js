a47c362e9795e18bfb7a75ab1840b2dfcdfbafb4
fvesp18 <1081@holbertonschool.com>
2020-05-13 01:01:34 +0000

    [UPDATE] Make semistandard compliant

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,16 +3,16 @@ const allArgs = process.argv.slice(2);
 const fs = require('fs');
 const fn = allArgs[0];
 
-fs.open(fn,'r', (err, fd) => {
-	if (err) {
-		console.log(err);
-	} else {
-		fs.readFile(fn, 'utf-8', (err, cont) => {
-			if (err) {
-				return console.log(err);
-			} else {
-				console.log(cont);
-			}
-		});
-	}
+fs.open(fn, 'r', (err, fd) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.readFile(fn, 'utf-8', (err, cont) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log(cont);
+      }
+    });
+  }
 });
