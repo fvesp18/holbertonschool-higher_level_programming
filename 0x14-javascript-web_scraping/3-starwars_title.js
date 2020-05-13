724451b2336787f6926b367fa9ab1ecf94edc94b
fvesp18 <1081@holbertonschool.com>
2020-05-13 01:41:39 +0000

    [UPDATE] Task 3, makes request from Star Wars API using path to get title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const req = require('request');
+const allArgs = process.argv.slice(2);
+const id = allArgs[0];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+req.get(url + id, function (err, r, cont) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(JSON.parse(cont).title);
+	}
+});
