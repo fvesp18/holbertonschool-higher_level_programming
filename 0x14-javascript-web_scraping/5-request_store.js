5d8ba947831134386a4f2ec6016b52aa9ea3b502
fvesp18 <1081@holbertonschool.com>
2020-05-13 01:57:37 +0000

    [UPDATE] Task 5, request file from URL, read and print to a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const req = require('request');
+const fs = require('fs');
+const allArgs = process.argv.slice(2);
+const fn = allArgs[1];
+const url = allArgs[0];
+
+req.get(url, function (err, r, cont) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(fn, cont, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
