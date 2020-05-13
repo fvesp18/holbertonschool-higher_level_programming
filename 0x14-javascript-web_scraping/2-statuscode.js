88c9db203f3ef30745a5085d5ca8a49a6fa2893d
fvesp18 <1081@holbertonschool.com>
2020-05-13 01:31:24 +0000

    [UPDATE] Task 2, requests status code from url

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const allArgs = process.argv.slice(2);
+const url = allArgs[0];
+const req = require('request');
+
+req.get(url, function (err, resp) {
+  if (err) {
+    console.log(err);
+  } else if (err || resp) {
+    console.log('code:', resp.statusCode);
+  }
+});
