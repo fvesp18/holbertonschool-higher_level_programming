a0f7b3274f6d2cacb20de60c3cccd1cb0d088863
fvesp18 <1081@holbertonschool.com>
2020-05-13 18:08:32 +0000

    [UPDATE] Task 2, convert function to =>

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const allArgs = process.argv.slice(2);
 const url = allArgs[0];
 const req = require('request');
 
-req.get(url, function (err, resp) {
+req.get(url, (err, resp) => {
   if (err || resp) {
     console.log('code:', resp.statusCode);
   }
