86b9556a47aab6d0f37a17613a111c17c5b44c4f
fvesp18 <1081@holbertonschool.com>
2020-05-15 03:26:01 +0000

    [UPDATE] Task 8, requests titles from api and append results as li

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+$.get("https://swapi-api.hbtn.io/api/films/?format=json", function ( data ) {
+	for (const title in data.results) {	
+		$('#list_movies').append('<li>' + data.results[title].title + '</li>')
+	}
+});
